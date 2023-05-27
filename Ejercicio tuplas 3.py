a26dc403edb564c410be38d3cdb31e4bade60773
GonzaloB02 <131916311+GonzaloB02@users.noreply.github.com>
2023-05-27 18:20:40 -0300

    Add files via upload

---
+++ Ejercicio tuplas 3.py
@@ -0,0 +1,9 @@
+#Ejercicio tuplas 3
+
+Tuplas = (1, 3, 4, 8, 2, 0, -5, 21, 9, -5, 22)
+
+Minimo = min(Tuplas)
+Maximo = max(Tuplas)
+
+print(f"El numero minimo de la tupla es {Minimo} y el maximo es {Maximo}")
+
